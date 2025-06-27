6f30de74a0906a129bdaf0d3b992c022fe81e2e0
syedarshadali-87 <sarshadali2025@gmail.com>
2025-06-28 00:42:14 +0500

    update

---
+++ dialogflow.js
@@ -0,0 +1,38 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const { WebhookClient, Suggestion } = require('dialogflow-fulfillment');
+const express = require("express")
+const cors = require("cors");
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.post("/webhook", async (req, res) => {
+    var id = (res.req.body.session).substr(43);
+    console.log(id)
+    const agent = new WebhookClient({ request: req, response: res });
+
+    function hi(agent) {
+        console.log(`intent  =>  hi`);
+        agent.add("hello from server")
+    }
+
+    function sendNotes(agent) {
+        const { number , date , email} = agent.parameters;
+       agent.add("")
+    }
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', hi); 
+    intentMap.set('sendNotes', sendNotes);
+    agent.handleRequest(intentMap);
+})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
